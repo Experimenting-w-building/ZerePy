1448ebb0d657527bc2a6f19a5ca54f7f8f01230b
Rashid Goshtasbi <45042692+rsgoshtasbi@users.noreply.github.com>
2025-01-08 21:26:03 -0600

    Added GitHub connection template and integrated with config

--- src/connection_manager.py
+++ src/connection_manager.py
@@ -9,6 +9,8 @@ from src.connections.farcaster_connection import FarcasterConnection
 from src.connections.ollama_connection import OllamaConnection
 from src.connections.echochambers_connection import EchochambersConnection
 from src.connections.hyperbolic_connection import HyperbolicConnection
+from src.connections.hyperbolic_connection import HyperbolicConnection
+from src.connections.github_connection import GitHubConnection
 
 logger = logging.getLogger("connection_manager")
 
@@ -36,7 +38,8 @@ class ConnectionManager:
             return EchochambersConnection
         elif class_name == "hyperbolic":
             return HyperbolicConnection
-
+        elif class_name == 'github':
+            return GitHubConnection
         return None
     
     def _register_connection(self, config_dic: Dict[str, Any]) -> None:
